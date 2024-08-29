29f46d251393e1d931e76ea750bd1a77a7589ad5
scarcemrk <karanbharda023@gmail.com>
2024-08-29 14:39:13 +0530

    added CORS config

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const cors = require('cors'); // Import the cors package
 const app = express();
 const discordRoutes = require('./routes/discordRoutes');
 
+// Enable CORS for all origins
+app.use(cors());
+
+// Define your routes
 app.use('/api', discordRoutes);
 
 module.exports = app;
+
